31d3a123d5b17f7257a55c95dd63d46b6e7720c5
Carl Little <98388123+carllittle1@users.noreply.github.com>
2022-03-20 12:09:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+const statsApiUrl = 'https://api.alphpool.net';
+
+function statsApiCall(action) {
+    return fetch(`${statsApiUrl}${action}`)
+      .then(response => response.json())
+}
+
+function fetchCurrencyInfo() {
+    return statsApiCall(`/rate`)
+}
+
+function fetchPoolHashrate() {
+    return statsApiCall(`/hashrate?period=3600`);
+}
+
+function fetchPoolProfit() {
+    return statsApiCall('/profit');
+}
+
+function fetchPoolSummary() {
+    return statsApiCall('/summary');
+}
+
+function showPoolHashrate(hashrate) {
+    const toHm = (h) => (parseFloat(h) / 1000000).toFixed(2)
+    document.getElementById('pool_hashrate').textContent = `${toHm(hashrate)} MH/s`
+}
+
+function showPoolProfit(profit) {
+    const roundProfit = parseFloat(profit).toFixed(4)
+    document.getElementById('pool_profit').textContent = `${roundProfit}`
+}
+
+function showPoolProfitUSD(rate, profit) {
+    const floatProfit = parseFloat(profit);
+    const floatRate = parseFloat(rate);
+    const profitUSD = (floatProfit * floatRate).toFixed(4);
+
+    document.getElementById('pool_profit_usd').textContent = `${profitUSD}`
+}
+
+function showPool24hBlocks(blocksCount) {
+    document.getElementById('24h_blocks').textContent = blocksCount
+}
+
+function showPoolLatestBlockAt(date) {
+    const current = new Date()
+    const at = new Date(date)
+    const hours = (Math.abs(current - at) / 36e5).toFixed(2);
+
+    document.getElementById('latest_block_at').textContent = `${hours} hour(s)`
+}
+
+function init() {
+    Promise.all(
+      [
+          fetchPoolHashrate(),
+          fetchPoolProfit(),
+          fetchPoolSummary()
+      ]
+    ).then((
+      [
+          { hashrate },
+          { profit },
+          summary,
+      ]
+    ) => {
+        showPoolHashrate(hashrate);
+        showPoolProfit(profit);
+        showPool24hBlocks(summary.blocks_24h);
+        showPoolLatestBlockAt(summary.last_block_at);
+
+        fetchCurrencyInfo().then(
+          ({ rate }) => showPoolProfitUSD(profit, rate)
+        )
+    });
+}
+
+init();
---
+++ stats.js
@@ -0,0 +1,140 @@
+const statsApiUrl = 'https://api.alphpool.net';
+
+function statsApiCall(action) {
+    return fetch(`${statsApiUrl}${action}`)
+      .then(response => response.json())
+}
+
+function fetchMyHashrate(wallet) {
+    return Promise.all(
+      [
+          statsApiCall(`/hashrate?worker=${wallet}&period=3600`),
+          statsApiCall(`/hashrate?worker=${wallet}&period=86400`),
+      ]
+    )
+}
+
+function fetchMyPayouts(wallet) {
+    return Promise.all(
+      [
+          statsApiCall(`/payouts?worker=${wallet}&period=3600`),
+          statsApiCall(`/payouts?worker=${wallet}&period=86400`),
+      ]
+    )
+}
+
+function fetchMyBalance(wallet) {
+    return statsApiCall(`/balance?worker=${wallet}`)
+}
+
+function showMyHashrate({ day, hour }) {
+    const toHm = (h) => (parseFloat(h) / 1000000).toFixed(2)
+
+    document.getElementById('my_hashrate_1h').textContent = toHm(hour.hashrate)
+    document.getElementById('my_hashrate_24h').textContent = toHm(day.hashrate)
+}
+
+function showMyPayouts({ day, hour }) {
+    document.getElementById('my_payouts_1h').textContent = parseFloat(hour.amount).toFixed(8)
+    document.getElementById('my_payouts_24h').textContent = parseFloat(day.amount).toFixed(8)
+}
+
+function showMyBalance(myBalanceData) {
+    document.getElementById('balance').textContent = parseFloat(myBalanceData.ready_to_pay).toFixed(8)
+}
+
+
+function drawData(wallet) {
+    disableButton();
+    Promise.all(
+      [
+          fetchMyHashrate(wallet),
+          fetchMyPayouts(wallet),
+          fetchMyBalance(wallet)
+      ]
+    ).then((
+      [
+          [hashrate1hResponse, hashrate24hResponse],
+          [payouts1hResponse, payouts24hResponse],
+          myBalanceResponse
+      ]
+    ) => {
+        showMyHashrate({ hour: hashrate1hResponse, day: hashrate24hResponse });
+        showMyPayouts({ hour: payouts1hResponse.payouts, day: payouts24hResponse.payouts });
+        showMyBalance(myBalanceResponse);
+        showStats();
+        enableButton();
+    });
+}
+
+function showStats() {
+    var element = document.getElementById("stats");
+    element.classList.remove("hidden");
+}
+
+function disableButton() {
+    const button = document.getElementById('show')
+    button.textContent = 'Loading..'
+    button.disabled = true
+}
+
+function enableButton() {
+    const button = document.getElementById('show')
+    button.textContent = 'Update'
+    button.disabled = false
+}
+
+function setWalletParam(wallet) {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    urlParams.set('wallet', wallet);
+
+    window.location.search = urlParams;
+}
+
+function getWalletParam() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('wallet');
+}
+
+function setWalletForm(wallet) {
+    const walletInput = document.getElementById('wallet-input');
+    walletInput.value = wallet;
+}
+
+function assignFormListener() {
+    function processForm(e) {
+        if (e.preventDefault) e.preventDefault();
+        const walletInput = document.getElementById('wallet-input');
+        if (walletInput.value)
+            setWalletParam(walletInput.value)
+        return false;
+    }
+
+    const form = document.getElementById('wallet-form');
+    if (form.attachEvent) {
+        form.attachEvent("submit", processForm);
+    } else {
+        form.addEventListener("submit", processForm);
+    }
+}
+
+function init() {
+    assignFormListener();
+
+    const walletFromParams = getWalletParam();
+
+    if (walletFromParams) {
+        Cookies.set('wallet', walletFromParams, { expires: 365 })
+        setWalletForm(walletFromParams);
+        drawData(walletFromParams);
+    } else {
+        const walletFromCookies = Cookies.get('wallet')
+        if (walletFromCookies) {
+            setWalletParam(walletFromCookies)
+        }
+    }
+}
+
+init();
